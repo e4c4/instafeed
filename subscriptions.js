2aad32c7be39ecacc53c414a9eee25cc33a56212
Edwin Chong <ekchong@yahoo.com>
2012-02-26 04:10:36 -0500

    configuring Redis for Heroku

--- subscriptions.js
+++ subscriptions.js
@@ -1,10 +1,10 @@
 if (process.env.REDISTOGO_URL) {
-	var redis = require('redis-url').createClient(process.env.REDISTOGO_URL);
+	//var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 	
-	//var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	//var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
-	//redis.auth(rtg.auth.split(":")[1]);
+	redis.auth(rtg.auth.split(":")[1]);
 } else {
 	var redis = require('redis');
 }
